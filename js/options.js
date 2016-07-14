93298135c9d05e52e24cfd792c526370da2ef66e
Calv Collins <calvcoll@gmail.com>
2016-07-14 15:42:55 +0100

    Updated with new babel outputs

--- js/options.js
+++ js/options.js
@@ -1,5 +1,9 @@
-var Button = React.createClass({displayName: "Button",
-    onClick : function() {
+"use strict";
+
+var Button = React.createClass({
+    displayName: "Button",
+
+    onClick: function onClick() {
         var inUse = this.props.current == this.props.name;
         if (!inUse) {
             this.props.setCurrent(this.props.name, this.props.url);
@@ -7,41 +11,46 @@ var Button = React.createClass({displayName: "Button",
             this.props.setCurrent("none", "none");
         }
     },
-   render: function() {
-       var rendername = this.props.name;
-       if (rendername.indexOf('-')>-1) {
-           rendername = rendername.split('-').join(' ');
-       }
-        return (
-            React.createElement("button", {className: "chrome-bootstrap", onClick: this.onClick}, 
-                rendername, " ",  + (this.props.current == this.props.name) ? " ✓" : ""
-            )
+    render: function render() {
+        var rendername = this.props.name;
+        if (rendername.indexOf('-') > -1) {
+            rendername = rendername.split('-').join(' ');
+        }
+        return React.createElement(
+            "button",
+            { className: "chrome-bootstrap", onClick: this.onClick },
+            rendername,
+            " ",
+            +(this.props.current == this.props.name) ? " ✓" : ""
         );
     }
 });
-var ToggleButton = React.createClass({displayName: "ToggleButton",
-    getInitialState: function() {
-        chrome.storage.sync.get(null, function(data) {
+var ToggleButton = React.createClass({
+    displayName: "ToggleButton",
+
+    getInitialState: function getInitialState() {
+        chrome.storage.sync.get(null, function (data) {
             if (data.current_add_on != undefined) {
                 var index = data.current_add_on.indexOf(this.props.name);
                 if (index >= 0) {
-                    this.setState({on : true});
+                    this.setState({ on: true });
                 }
             }
         }.bind(this));
         return {
-            on : false
-        }
+            on: false
+        };
     },
-    onClick: function() {
+    onClick: function onClick() {
         var oldState = this.state.on;
         var url = this.props.url;
         var name = this.props.name;
-        this.setState({ on : !oldState });
+        this.setState({ on: !oldState });
         var in_list = false;
         var list = [];
         var url_list = [];
-        chrome.storage.sync.get(null, function(data) { //active
+        chrome.storage.sync.get(null, function (data) {
+            //active
             list = data.current_add_on;
             url_list = data.current_add_on_url;
             if (list != undefined) {
@@ -55,172 +64,237 @@ var ToggleButton = React.createClass({displayName: "ToggleButton",
             url_list = url_list == undefined ? [] : url_list;
             var new_list = list;
             var new_url_list = url_list;
-            if (oldState && in_list) { //no oldState && !inList as no change needed
+            if (oldState && in_list) {
+                //no oldState && !inList as no change needed
                 var index = list.indexOf(this.props.name);
                 if (index >= 0) {
                     new_list = list.splice(index, 1);
                     new_url_list = url_list.splice(index, 1);
                 }
-            } else if (!oldState && !in_list) { //no !oldstate && in_list as addon is needed
+            } else if (!oldState && !in_list) {
+                //no !oldstate && in_list as addon is needed
                 new_list.push(this.props.name);
                 new_url_list.push(this.props.url);
             }
             chrome.storage.sync.set({
-                current_add_on : new_list,
-                current_add_on_url : new_url_list
+                current_add_on: new_list,
+                current_add_on_url: new_url_list
             });
-            chrome.tabs.query({url: '*://tweetdeck.twitter.com/*'}, function(tabs) {
-                tabs.forEach(function(tab) {
+            chrome.tabs.query({ url: '*://tweetdeck.twitter.com/*' }, function (tabs) {
+                tabs.forEach(function (tab) {
                     chrome.tabs.sendMessage(tab.id, {
-                        addon_url : url,
-                        addon_name : name,
-                        addon_on : !oldState
-                    }, function(){
+                        addon_url: url,
+                        addon_name: name,
+                        addon_on: !oldState
+                    }, function () {
                         //console.log("message sent:" + !oldState + " " + name + " " + url);
                     });
                 });
             });
         }.bind(this));
     },
-    render: function() {
+    render: function render() {
         var renderName = this.props.name;
-        if (renderName.indexOf('-')>-1) {
+        if (renderName.indexOf('-') > -1) {
             renderName = renderName.split('-').join(' ');
         }
-        return (
-            React.createElement("button", {className: "chrome-bootstrap toggle", onClick: this.onClick}, 
-                renderName, " ",  + (this.state.on) ? " ✓" : ""
-            )
-        )
+        return React.createElement(
+            "button",
+            { className: "chrome-bootstrap toggle", onClick: this.onClick },
+            renderName,
+            " ",
+            +this.state.on ? " ✓" : ""
+        );
     }
 });
-var StyleMenu = React.createClass({displayName: "StyleMenu",
-    getInitialState: function() {
+var StyleMenu = React.createClass({
+    displayName: "StyleMenu",
+
+    getInitialState: function getInitialState() {
         var menu = this;
         var current = "";
-        chrome.storage.sync.get(null, function(data) { //gets sync data -> current button
+        chrome.storage.sync.get(null, function (data) {
+            //gets sync data -> current button
             if (data.name != undefined && data.url != undefined) {
                 menu.setCurrent(data.name, data.url);
             }
         });
         return {
-            online : false,
-            option : "",
-            current : current
-        }
+            online: false,
+            option: "",
+            current: current
+        };
     },
-    componentDidMount: function() {
+    componentDidMount: function componentDidMount() {
         //$.get(chrome.extension.getURL("popup.html"), function() {
-        $.get('https://raw.githubusercontent.com/calvcoll/rhodochronsite/master/rhodo.css', function() { // using rhodo as is hosted on github (won't re-direct)
+        $.get('https://raw.githubusercontent.com/calvcoll/rhodochronsite/master/rhodo.css', function () {
+            // using rhodo as is hosted on github (won't re-direct)
             this.setState({
                 online: true
             });
         }.bind(this));
     },
-    setCurrent: function(style_name,style_url) {
-        this.setState({current: style_name});
+    setCurrent: function setCurrent(style_name, style_url) {
+        this.setState({ current: style_name });
         chrome.storage.sync.set({
-            name : style_name,
-            url : style_url
+            name: style_name,
+            url: style_url
         });
 
-        chrome.tabs.query({url: '*://tweetdeck.twitter.com/*'}, function(tabs) {
-            tabs.forEach(function(tab) {
+        chrome.tabs.query({ url: '*://tweetdeck.twitter.com/*' }, function (tabs) {
+            tabs.forEach(function (tab) {
                 chrome.tabs.sendMessage(tab.id, {
-                    url : style_url,
-                    name : style_name
-                }, function(){
+                    url: style_url,
+                    name: style_name
+                }, function () {
                     //console.log("message sent:" + style_name + " " + style_url);
                 });
             });
         });
     },
-    render: function() {
-        return (
-            React.createElement("div", {className: "buttonContainer"}, 
-                React.createElement("div", {className: "online"}, " Online: ", React.createElement("div", {className: "online-icon " + (this.state.online ? 'online' : 'offline')})), 
-                React.createElement(Button, {url: "https://raw.githubusercontent.com/calvcoll/rhodochronsite/master/rhodo.css", name: "Rhodochronsite", current: this.state.current, setCurrent: this.setCurrent}), 
-                React.createElement(Button, {url: "https://raw.githubusercontent.com/WinterReign/Snowflake/master/snowflake%20classic%20black.css", name: "Snowflake-Classic-Black", current: this.state.current, setCurrent: this.setCurrent}), 
-                React.createElement(Button, {url: "https://raw.githubusercontent.com/WinterReign/Snowflake/master/snowflake%20classic%20light.css", name: "Snowflake-Classic-Light", current: this.state.current, setCurrent: this.setCurrent}), 
-                React.createElement(Button, {url: "https://raw.githubusercontent.com/WinterReign/Snowflake/master/snowflake%20minima%20synth.css", name: "Snowflake-Minima-Synth", current: this.state.current, setCurrent: this.setCurrent}), 
-                React.createElement(Button, {url: "https://raw.githubusercontent.com/FractalHexagon/Snowflake/master/snowflake%20minima%20black%20aero.css", name: "Snowflake-Minima-Black-Aero", current: this.state.current, setCurrent: this.setCurrent}), 
-                React.createElement(Button, {url: "https://raw.githubusercontent.com/FractalHexagon/Snowflake/master/snowflake%20minima%20white%20aqua.css", name: "Snowflake-Minima-White-Aqua", current: this.state.current, setCurrent: this.setCurrent}), 
-                React.createElement("h1", null, "Add-ons"), React.createElement("hr", null), 
-                React.createElement(ToggleButton, {url: "https://gist.githubusercontent.com/pixeldesu/1afcc9d3978d04bd3646/raw/9130ecd344a66b24009e915b8b74e9876e70b919/direct-message.scss", name: "Direct-Message-Hiding"}), 
-                React.createElement(ToggleButton, {url: "https://raw.githubusercontent.com/pixeldesu/visuals/master/tweetdeck/stars/stars.css", name: "Force-Old-Stars"}), 
-                React.createElement(ToggleButton, {url: "https://raw.githubusercontent.com/FractalHexagon/Snowflake/master/force-normal-case.css", name: "Force-Default-Case"})
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "buttonContainer" },
+            React.createElement(
+                "div",
+                { className: "online" },
+                " Online: ",
+                React.createElement("div", { className: "online-icon " + (this.state.online ? 'online' : 'offline') })
+            ),
+            React.createElement(Button, { url: "https://raw.githubusercontent.com/calvcoll/rhodochronsite/master/rhodo.css", name: "Rhodochronsite", current: this.state.current, setCurrent: this.setCurrent }),
+            React.createElement(Button, { url: "https://raw.githubusercontent.com/WinterReign/Snowflake/master/snowflake%20classic%20black.css", name: "Snowflake-Classic-Black", current: this.state.current, setCurrent: this.setCurrent }),
+            React.createElement(Button, { url: "https://raw.githubusercontent.com/WinterReign/Snowflake/master/snowflake%20classic%20light.css", name: "Snowflake-Classic-Light", current: this.state.current, setCurrent: this.setCurrent }),
+            React.createElement(Button, { url: "https://raw.githubusercontent.com/WinterReign/Snowflake/master/snowflake%20minima%20synth.css", name: "Snowflake-Minima-Synth", current: this.state.current, setCurrent: this.setCurrent }),
+            React.createElement(Button, { url: "https://raw.githubusercontent.com/FractalHexagon/Snowflake/master/snowflake%20minima%20black%20aero.css", name: "Snowflake-Minima-Black-Aero", current: this.state.current, setCurrent: this.setCurrent }),
+            React.createElement(Button, { url: "https://raw.githubusercontent.com/FractalHexagon/Snowflake/master/snowflake%20minima%20white%20aqua.css", name: "Snowflake-Minima-White-Aqua", current: this.state.current, setCurrent: this.setCurrent }),
+            React.createElement(
+                "h1",
+                null,
+                "Add-ons"
+            ),
+            React.createElement("hr", null),
+            React.createElement(ToggleButton, { url: "https://gist.githubusercontent.com/pixeldesu/1afcc9d3978d04bd3646/raw/9130ecd344a66b24009e915b8b74e9876e70b919/direct-message.scss", name: "Direct-Message-Hiding" }),
+            React.createElement(ToggleButton, { url: "https://raw.githubusercontent.com/pixeldesu/visuals/master/tweetdeck/stars/stars.css", name: "Force-Old-Stars" }),
+            React.createElement(ToggleButton, { url: "https://raw.githubusercontent.com/FractalHexagon/Snowflake/master/force-normal-case.css", name: "Force-Default-Case" })
+        );
+    }
+});
+var ScriptMenu = React.createClass({
+    displayName: "ScriptMenu",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            null,
+            "It works!"
+        );
+    }
+});
+var NavBar = React.createClass({
+    displayName: "NavBar",
+
+    render: function render() {
+        var renderElements = [];
+        this.props.menus.forEach(function (menu) {
+            if (menu.props.id == this.props.default_id) {
+                renderElements.push(React.createElement(
+                    "li",
+                    { key: menu.props.id, className: "selected" },
+                    React.createElement(
+                        "a",
+                        { href: "#" + menu.props.id },
+                        menu.props.human_id
+                    )
+                ));
+            } else {
+                renderElements.push(React.createElement(
+                    "li",
+                    { key: menu.props.id },
+                    React.createElement(
+                        "a",
+                        { href: "#" + menu.props.id },
+                        menu.props.human_id
+                    )
+                ));
+            }
+        }.bind(this));
+        return React.createElement(
+            "div",
+            { className: "navigation" },
+            React.createElement(
+                "div",
+                { className: "logo" },
+                React.createElement("img", { className: "logo", src: "img/snowflake_logo-48.png", alt: "snowflake-logo" }),
+                React.createElement(
+                    "h2",
+                    null,
+                    "Snwflk"
+                )
+            ),
+            React.createElement(
+                "ul",
+                { className: "menu" },
+                renderElements
             )
         );
     }
 });
-var ScriptMenu = React.createClass({displayName: "ScriptMenu",
-    render: function() {
-        return (
-            React.createElement("div", null, 
-                "It works!"
-            )
-        )
-    }
-});
-var NavBar = React.createClass({displayName: "NavBar",
-    render: function () {
+var MainView = React.createClass({
+    displayName: "MainView",
+
+    render: function render() {
         var renderElements = [];
-        this.props.menus.forEach(function(menu) {
+        this.props.menus.forEach(function (menu) {
             if (menu.props.id == this.props.default_id) {
-                renderElements.push(React.createElement("li", {key: menu.props.id, className: "selected"}, React.createElement("a", {href: "#"+ menu.props.id}, menu.props.human_id)));
-            }
-            else {
-                renderElements.push(React.createElement("li", {key: menu.props.id}, React.createElement("a", {href: "#"+ menu.props.id}, menu.props.human_id)));
+                renderElements.push(React.createElement(
+                    "div",
+                    { key: menu.props.id, id: menu.props.id, className: "selected" },
+                    React.createElement(
+                        "h1",
+                        null,
+                        menu.props.human_id
+                    ),
+                    React.createElement("hr", null),
+                    menu
+                ));
+            } else {
+                renderElements.push(React.createElement(
+                    "div",
+                    { key: menu.props.id, id: menu.props.id },
+                    React.createElement(
+                        "h1",
+                        null,
+                        menu.props.human_id
+                    ),
+                    React.createElement("hr", null),
+                    menu
+                ));
             }
         }.bind(this));
-        return (
-            React.createElement("div", {className: "navigation"}, 
-                React.createElement("div", {className: "logo"}, 
-                    React.createElement("img", {className: "logo", src: "img/snowflake_logo-48.png", alt: "snowflake-logo"}), 
-                    React.createElement("h2", null, "Snwflk")
-                ), 
-                React.createElement("ul", {className: "menu"}, 
-                    renderElements
-                )
-            )
-        )
+        return React.createElement(
+            "div",
+            { className: "mainview view" },
+            renderElements
+        );
     }
 });
-var MainView = React.createClass({displayName: "MainView",
-    render: function() {
-        var renderElements = [];
-        this.props.menus.forEach(function(menu) {
-            if (menu.props.id == this.props.default_id) {
-                renderElements.push(React.createElement("div", {key: menu.props.id, id: menu.props.id, className: "selected"}, React.createElement("h1", null, menu.props.human_id), React.createElement("hr", null), menu));
-            }
-            else {
-                renderElements.push(React.createElement("div", {key: menu.props.id, id: menu.props.id}, React.createElement("h1", null, menu.props.human_id), React.createElement("hr", null), menu));
-            }
-        }.bind(this));
-        return (
-            React.createElement("div", {className: "mainview view"}, 
-                renderElements
-            )
-        )
-    }
-});
-var Frame = React.createClass({displayName: "Frame",
-    getInitialState: function() {
+var Frame = React.createClass({
+    displayName: "Frame",
+
+    getInitialState: function getInitialState() {
         return {
-            default_id : "styleSwitcher",
-            menus : [
-                React.createElement(StyleMenu, {id: "styleSwitcher", human_id: "Styles"}),
-                React.createElement(ScriptMenu, {id: "jsMenu", human_id: "Javascript"})
-            ]
-        }
+            default_id: "styleSwitcher",
+            menus: [React.createElement(StyleMenu, { id: "styleSwitcher", human_id: "Styles" }), React.createElement(ScriptMenu, { id: "jsMenu", human_id: "Javascript" })]
+        };
     },
-    render: function() {
-        return (
-            React.createElement("div", {className: "frame"}, 
-            React.createElement(NavBar, {menus: this.state.menus, default_id: this.state.default_id}), 
-            React.createElement(MainView, {menus: this.state.menus, default_id: this.state.default_id})
-            )
-        )
+    render: function render() {
+        return React.createElement(
+            "div",
+            { className: "frame" },
+            React.createElement(NavBar, { menus: this.state.menus, default_id: this.state.default_id }),
+            React.createElement(MainView, { menus: this.state.menus, default_id: this.state.default_id })
+        );
     }
 });
 React.render(React.createElement(Frame, null), document.body);
--- js/popup.js
+++ js/popup.js
@@ -1,68 +1,74 @@
-var Button = React.createClass({displayName: "Button",
-    getInitialState: function() {
-        chrome.storage.sync.get('toggle', function(data) { //gets sync data -> current button
-            if ((data.toggle == true || data.toggle == false) &&
-            (data.toggle != undefined || data.toggle != null)) {
-                this.setState({on: data.toggle}); // I have no idea why it has to be opposite but I'm not moaning.
+"use strict";
+
+var Button = React.createClass({
+    displayName: "Button",
+
+    getInitialState: function getInitialState() {
+        chrome.storage.sync.get('toggle', function (data) {
+            //gets sync data -> current button
+            if ((data.toggle == true || data.toggle == false) && (data.toggle != undefined || data.toggle != null)) {
+                this.setState({ on: data.toggle }); // I have no idea why it has to be opposite but I'm not moaning.
             }
         }.bind(this));
         return {
             on: true
-        }
+        };
     },
-    iconToggle: function () {
+    iconToggle: function iconToggle() {
         var icon = !this.state.on ? "img/snowflake_logo-48.png" : "img/snowflake_logo-48-disabled.png";
-        chrome.browserAction.setIcon({path: icon});
+        chrome.browserAction.setIcon({ path: icon });
 
         $('#logo').attr("src", icon);
     },
-    toggle: function() {
-        this.setState({on: !this.state.on});
-        chrome.storage.sync.set({toggle: !this.state.on});
+    toggle: function toggle() {
+        this.setState({ on: !this.state.on });
+        chrome.storage.sync.set({ toggle: !this.state.on });
         this.iconToggle();
     },
-    onClick: function() {
+    onClick: function onClick() {
         this.toggle();
-        chrome.tabs.query({url: '*://tweetdeck.twitter.com/*'}, function(tabs) {
-            tabs.forEach(function(tab) {
+        chrome.tabs.query({ url: '*://tweetdeck.twitter.com/*' }, function (tabs) {
+            tabs.forEach(function (tab) {
                 chrome.tabs.sendMessage(tab.id, {
-                    toggle : this.state.on
-                }, function(){
+                    toggle: this.state.on
+                }, function () {
                     console.log("message sent: toggle");
                 });
             });
         }.bind(this));
     },
-   render: function() {
-        return (
-            React.createElement("button", {onClick: this.onClick, className: (this.state.on) ? "inUse" : ""}, 
-                this.props.name, 
-                React.createElement("br", null)
-            )
+    render: function render() {
+        return React.createElement(
+            "button",
+            { onClick: this.onClick, className: this.state.on ? "inUse" : "" },
+            this.props.name,
+            React.createElement("br", null)
         );
     }
 });
-var Menu = React.createClass({displayName: "Menu",
-   getInitialState: function() {
-       var current = "";
-       return {
-           online : false,
-           option : "",
-           current : current
-       }
-   },
-    componentDidMount: function() {
-        $.get(chrome.extension.getURL("popup.html"), function() {
+var Menu = React.createClass({
+    displayName: "Menu",
+
+    getInitialState: function getInitialState() {
+        var current = "";
+        return {
+            online: false,
+            option: "",
+            current: current
+        };
+    },
+    componentDidMount: function componentDidMount() {
+        $.get(chrome.extension.getURL("popup.html"), function () {
             this.setState({
                 online: true
             });
         }.bind(this));
     },
-    render: function() {
-        return (
-            React.createElement("p", null, 
-                React.createElement(Button, {name: "Toggle"})
-            )
+    render: function render() {
+        return React.createElement(
+            "p",
+            null,
+            React.createElement(Button, { name: "Toggle" })
         );
     }
 });
